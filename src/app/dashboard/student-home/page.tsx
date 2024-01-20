637affb30b3071212a7122b4bebe6efe1a86efae
Sill <moonstone.lunaria@gmail.com>
2024-01-20 11:07:51 +0800

    upload a page

--- src/app/dashboard/student-home/page.tsx
+++ src/app/dashboard/student-home/page.tsx
@@ -97,7 +97,6 @@ export default function Page() {
                         </p>
                       </div>
                     </div>
-                    
                   </div>
                 </div>
               </div>
